ae232dc71433f50a8af0466ba297ed5c5bc9c4d2
123123 <a123123@123123deMacBook-Air.local>
2024-04-23 16:28:52 +0800

    去掉不必要的变量

--- src/routes/(app)/dataset/+page.svelte
+++ src/routes/(app)/dataset/+page.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
 	import { accounts, mails } from "./data.js";
 	import Mail from "./(components)/mail.svelte";
-
-	export let data;
 </script>
 
 <Mail
 	{accounts}
 	{mails}
-	defaultLayout={data.layout}
-	defaultCollapsed={data.collapsed}
 	navCollapsedSize={4}
 />
