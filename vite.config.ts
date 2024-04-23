7f3f2e0da29652b6b9e80e6147d17d7727fce7f8
123123 <a123123@123123deMacBook-Air.local>
2024-04-23 10:28:16 +0800

    更新到了v0.1.120;package.json更新到了最新版本

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,24 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+	define: {
+		APP_VERSION: JSON.stringify(process.env.npm_package_version)
+	},	
+	server: {
+		proxy: {
+		 '/api': {
+		   target: 'http://localhost:8080', // 目标后端服务地址
+		   changeOrigin: true, // 是否改变请求源
+		   rewrite: (path) => path.replace(/^\/api/, ''), // 重写请求路径
+		},
+		 '/manifest.json': {
+		   target: 'http://localhost:8080',
+		   changeOrigin: true,
+		   rewrite: (path) => '/manifest.json'//path.replace(/^\/manifest\.json/, ''),
+		},
+		 // ...其他代理规则
+		},
+		// 其他server配置
+	},	
 });
