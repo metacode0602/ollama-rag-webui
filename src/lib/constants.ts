2cb2389d51ee44eabfdaec691de55bf36049d068
123123 <a123123@123123deMacBook-Air.local>
2024-05-07 16:35:02 +0800

    修改api访问路径

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ import { dev } from '$app/environment';
 // import { version } from '../../package.json';
 
 export const APP_NAME = 'Open WebUI';
-export const WEBUI_BASE_URL = dev ? `dev-api` : ``;
+export const WEBUI_BASE_URL = dev ? `/dev-api` : ``;
 
 export const WEBUI_API_BASE_URL = `${WEBUI_BASE_URL}/api/v1`;
 
